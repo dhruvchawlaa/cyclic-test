4c0a9e0659b06e701c2fb4deb7e6d407d2fa11e5
Dhruv Chawla <65937412+dhruvchawlaa@users.noreply.github.com>
2023-01-12 17:45:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Dhruv Chawla!");
+    res.send("Dhruv Chawla - dchawla3@myseneca.ca");
 });
 
 // setup http server to listen on HTTP_PORT
